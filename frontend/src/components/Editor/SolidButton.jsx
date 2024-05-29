7c7d87c3968f510e558fe76ee3ec74e2eebb46ab
talalcringe <104639094+talalcringe@users.noreply.github.com>
2024-05-29 16:15:44 +0500

    Revert "Talal editor"

--- frontend/src/components/Editor/SolidButton.jsx
+++ frontend/src/components/Editor/SolidButton.jsx
@@ -1,10 +1,9 @@
-const SolidButton = ({ text, color, onClick, infoText }) => {
+const SolidButton = ({ text, color, onClick }) => {
   return (
     <>
       <button
         onClick={onClick}
         className={`flex items-center justify-center font-bold p-2 m-2 rounded text-ghostWhite transition hover:scale-105 ${color}`}
-        title={infoText}
       >
         <span className='flex justify-center items-center gap-2'>{text}</span>
       </button>
